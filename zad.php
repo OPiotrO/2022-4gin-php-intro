8bfc44e2646fca90a879fdc3dd5d56ca81e9a1ca
OPiotrO <piotr2489@wp.pl>
2022-09-20 09:44:48 +0200

    zad3

--- zad.php
+++ zad.php
@@ -18,4 +18,43 @@ if ($input == NULL){
     natcasesort($matches);
     print_r($matches);
 }
+$kolumny = 4;
+$h = 4;
+$li_array = count($array);
+$li_4 = ceil($li_array/4);
+$j = 0;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lorem ipsum</title>
+    </head>
+    <body>
+        <table>
+        <?php    
+        for ($h = 0; $h < $li_4; $h++){
+        echo '<tr>';
+            for ($i = 0; $i < $kolumny; $i++) {
+            echo '<td>';
+                if ($j < $li_array){
+                echo $array[$j];
+                $j=$j+1;
+                };
+            echo '</td>';
+            };
+        echo '</tr>';
+        };
+        ?>
+        </table>
+    </body>
+</html>
+<style>
+table, th, td {
+  border: 1px solid;
+  border-collapse: collapse;
+}
+</style>
